3ef70aba491fa1a73b6d7d894109e6ee3c5ca8bd
jungdaru <jung.daru42@gmail.com>
2022-10-14 01:43:51 +0900

    doc file upload

--- src/test.py
+++ src/test.py
@@ -1,3 +1,10 @@
-print("Hello world")
+import discord
+from discord.ext import commands
 
-print("discord bot")
+client = commands.Bot(command_prefix='',intents = discord.Intents.all())
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('안녕하세요')
+
+client.run('MTAyNzA3NzcwNjk3MTYyNzU4MA.GwhgT3.3l6Aeqgd1kN7Y2u_oTdOQGUlx7KFL-4lbr_rKc')
